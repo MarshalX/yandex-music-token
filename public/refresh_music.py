d46a7d69dbb12db0d0aa0341eab3fa88084a5079
Aphanasiy <M79150306109@ya.ru>
2020-06-21 02:23:45 +0300

    Added some voices

--- public/refresh_music.py
+++ public/refresh_music.py
@@ -1,10 +1,15 @@
 import os
 
+make_prefix = lambda x: x[x.find(random_voices_folder):]
+random_voices_folder = "random_voices"
+
+def walker():
+    global make_prefix, random_voices_folder
+    for d, dirs, files in os.walk(os.path.join(os.getcwd(), random_voices_folder)):
+        for f in files:
+            yield os.path.join(make_prefix(d), f).replace('\\', '/')
+    
 
 fin = open("previews.txt", 'w')
-random_voices_folder = "random_voices"
-make_prefix = lambda x: x[x.find(random_voices_folder):]
-for d, dirs, files in os.walk(os.path.join(os.getcwd(), random_voices_folder)):
-    for f in files:
-        print(os.path.join(make_prefix(d), f).replace('\\', '/'), file=fin)
+fin.write('\n'.join(sorted(walker())))
 fin.close()
