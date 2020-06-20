a98e94fc6aba66926d8cfc9b475c9dc248f6d447
Aphanasiy <M79150306109@ya.ru>
2020-06-21 01:35:42 +0300

    previews.txt теперь пригоден для URL

--- public/refresh_music.py
+++ public/refresh_music.py
@@ -6,5 +6,5 @@ random_voices_folder = "random_voices"
 make_prefix = lambda x: x[x.find(random_voices_folder):]
 for d, dirs, files in os.walk(os.path.join(os.getcwd(), random_voices_folder)):
     for f in files:
-        print(os.path.join(make_prefix(d), f), file=fin)
+        print(os.path.join(make_prefix(d), f).replace('\\', '/'), file=fin)
 fin.close()
