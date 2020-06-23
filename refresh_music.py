fd93ae2ddc18a8fede7d4c8ba4f4afe579fa1cb5
Auto Delpoy <auto@deploy>
2020-06-23 09:56:57 +0000

    Updates

--- refresh_music.py
+++ refresh_music.py
@@ -1,10 +1,14 @@
 import os
 
 
-fin = open("previews.txt", 'w')
-random_voices_folder = "random_voices"
-make_prefix = lambda x: x[x.find(random_voices_folder):]
-for d, dirs, files in os.walk(os.path.join(os.getcwd(), random_voices_folder)):
-    for f in files:
-        print(os.path.join(make_prefix(d), f), file=fin)
-fin.close()
+def walker(folder):
+    for d, _, files in os.walk(os.path.join(os.getcwd(), folder)):
+        for file in files:
+            yield os.path.join(d[d.find(folder):], file).replace('\\', '/')
+
+
+if __name__ == '__main__':
+    RANDOM_VOICES_FOLDER = 'random_voices'
+
+    with open('previews.txt', 'w') as f:
+        f.write('\n'.join(sorted(walker(RANDOM_VOICES_FOLDER))))
